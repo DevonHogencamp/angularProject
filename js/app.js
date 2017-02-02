bdb06a68b56b58691ae4be9075cc0819f1d59c2d
Devon Hogencamp <devhogen1@gmail.com>
2017-02-02 15:28:46 -0700

    starting to add details page

--- js/app.js
+++ js/app.js
@@ -12,9 +12,14 @@ var myApp = angular.module('myApp', [
 // Config our app to use routeProvider
 myApp.config(['$routeProvider', function ($routeProvider) {
     // Config our routes
-    $routeProvider.when('/list', {
+    $routeProvider.
+    when('/list', {
         templateUrl: 'partials/list.html',
         controller: 'ListController'
+    }).
+    when('/details/:itemId', {
+        templateUrl: 'partials/details.html',
+        controller: 'DetailsController'
     }).
     otherwise({
         redirectTo: '/list'
--- js/controllers.js
+++ js/controllers.js
@@ -10,3 +10,13 @@ artistControllers.controller('ListController', ['$scope', '$http', function List
             console.log('HTTP request failed with result: ' + err);
         });
 }]);
+
+artistControllers.controller('DetailsController', ['$scope', '$http', function ListController($scope, $http) {
+    $http.get('js/data.json')
+        .then(function (res) {
+            $scope.artists = res.data;
+            $scope.whichItem = $routeParams.itemId;
+        }, function (err) {
+            console.log('HTTP request failed with result: ' + err);
+        });
+}]);
