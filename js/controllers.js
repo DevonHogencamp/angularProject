840c3f20a1f390d65f46ece408c5707c23b5f963
Devon Hogencamp <devhogen1@gmail.com>
2017-02-03 15:26:31 -0700

    fixed the link to the details page and put more info in the details with a back arrow and a way to go back and forth between people

--- js/controllers.js
+++ js/controllers.js
@@ -11,11 +11,27 @@ artistControllers.controller('ListController', ['$scope', '$http', function List
         });
 }]);
 
-artistControllers.controller('DetailsController', ['$scope', '$http', function ListController($scope, $http) {
+artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function ListController($scope, $http, $routeParams) {
     $http.get('js/data.json')
         .then(function (res) {
             $scope.artists = res.data;
             $scope.whichItem = $routeParams.itemId;
+
+            // Set $scope variable for next and prev array items
+            if ($routeParams.itemId > 0) {
+                $scope.prevItem = Number($routeParams.itemId) - 1;
+            }
+            else {
+                $scope.prevItem = $scope.artists.length - 1;
+            }
+
+            if ($routeParams.itemId < $scope.artists.length - 1) {
+                $scope.nextItem = Number($routeParams.itemId) + 1;
+            }
+            else {
+                $scope.nextItem = 0;
+            }
+
         }, function (err) {
             console.log('HTTP request failed with result: ' + err);
         });
